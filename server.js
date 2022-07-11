7491ceec35e552aff40ddefefbfb427772add173
Katie Eells <katieeells@Katies-MBP.lan>
2022-07-11 16:54:33 -0400

    in class activities complete

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// ROOT
+// // SEQUELIZE CONNECTION
+// const sequelize = new Sequelize({
+//     storage: process.env.PG_URI,
+//     dialect: 'postgres',
+//     username: 'postgres',
+//     password: '100K'
+//   })
+
+//   try {
+//     sequelize.authenticate()
+//     // const models = require('./models/band')
+//     console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
+//   } catch(err) {
+//     console.log(`Unable to connect to PG: ${err}`)
+//   }
+  
+// GET ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the Tour API'
